99b11680dca0209e35e5b61804adf08dd911483b
ayman <aymanrafeeque419@gmail.com>
2025-10-21 01:20:57 +0530

    GREEN: Implements test case for lose game by getting chances left 0

--- main.go
+++ main.go
@@ -61,7 +61,7 @@ func getSecretWord(wordFileName string) string {
 }
 
 func checkGuess(state Game, guess byte) Game {
-	if state.chancesLeft > 1 && !bytes.Contains(state.guesses, []byte{guess}) {
+	if state.chancesLeft > 0 && !bytes.Contains(state.guesses, []byte{guess}) {
 		if strings.ContainsRune(state.secretWord, rune(guess)) {
 			state.correctGuesses = append(state.correctGuesses, guess)
 			state.guesses = append(state.guesses, guess)
@@ -83,6 +83,58 @@ func hasWon(state Game) bool {
 
 }
 
+func hasLose(state Game) bool {
+	return state.chancesLeft == 0 && !hasWon(state)
+}
+
+func displayWord(state Game) {
+	for _, ch := range state.secretWord {
+		if bytes.Contains(state.correctGuesses, []byte{byte(ch)}) {
+			fmt.Printf("%c ", ch)
+		} else {
+			fmt.Print("_ ")
+		}
+	}
+	fmt.Println()
+}
+
 func main() {
-	fmt.Println(getSecretWord("/usr/share/dict/words"))
+	word := getSecretWord("/usr/share/dict/words")
+	game := NewGame(word)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Welcome to Hangman!")
+	fmt.Println("Try to guess the word! You have 7 chances.")
+	fmt.Println()
+
+	for game.chancesLeft > 0 {
+		displayWord(game)
+		fmt.Printf("Chances left: %d\n", game.chancesLeft)
+		fmt.Print("Enter a letter: ")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		if len(input) != 1 {
+			fmt.Println("Please enter only one letter.")
+			continue
+		}
+
+		guess := input[0]
+
+		if bytes.Contains(game.guesses, []byte{guess}) {
+			fmt.Printf("You already guessed '%c'. Try a different letter.\n\n", guess)
+			continue
+		}
+
+		game = checkGuess(game, input[0])
+
+		if hasWon(game) {
+			fmt.Println("\nCongratulations! You guessed the word:", game.secretWord)
+			return
+		}
+		fmt.Println()
+	}
+
+	fmt.Println("\nGame Over! The word was:", game.secretWord)
+
 }
